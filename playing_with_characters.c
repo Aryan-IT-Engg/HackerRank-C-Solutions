8630a00ec28d89e44e395346a4beb08f4cd9dc0b
Aryan-IT-Engg <aryanshendage0626@gmail.com>
2026-01-10 21:52:04 +0530

    Create playing_with_characters.c

---
+++ playing_with_characters.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    char ch;
+    char s[100];
+    char sen[100];
+    scanf("%c",&ch);
+    printf("%c\n",ch);
+    scanf("%s",&s);
+    printf("%s\n",s);
+    scanf(" %[^\n]%*c", sen);
+    printf("%s\n",sen);
+    return 0;
+}
